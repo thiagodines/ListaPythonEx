63fee78adbb83b7c63708e0066d54745b9b0a699
Thiago Callegari <92542640+thiagodines@users.noreply.github.com>
2022-05-02 15:43:53 -0300

    Add files via upload

---
+++ ListaPython/Sequencial/ex1.py
@@ -0,0 +1,11 @@
+"""
+Faça um Programa que peça dois números e imprima a soma.
+"""
+n1 = float(input('Digite um numero: '))
+n2 = float(input('Digite outro numero: '))
+soma = n1 + n2
+print()
+print(f'A soma dos números {n1} e {n2} é {soma}')
+
+
+
---
+++ ListaPython/Sequencial/ex3.py
@@ -0,0 +1,10 @@
+"""
+Faça um Programa que converta metros para centímetros.
+"""
+
+metros = int(input('Digite a quantidade de metros que deseja converter em centimetros: '))
+centimetros = metros*100
+print()
+print(f'{metros}m convertidos em centímetros é igual a {centimetros}cm!')
+
+
