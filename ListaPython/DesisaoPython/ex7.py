6f7362ec57a6fa17819378519be2cd3b30e86f46
Thiago Callegari <92542640+thiagodines@users.noreply.github.com>
2022-05-08 18:53:16 -0300

    Update ex7.py

--- ListaPython/DesisaoPython/ex7.py
+++ ListaPython/DesisaoPython/ex7.py
@@ -1,1 +1,27 @@
+"""
+Faça um Programa que leia três números e mostre o maior e o menor deles. 
+"""
 
+num_1 = int(input('Type the first number: '))
+num_2 = int(input('Type the second number: '))
+num_3 = int(input('Type the third number: '))
+
+# Maior número:
+maior = num_1
+if num_2 > maior:
+    maior = num_2
+
+if num_3 > maior:
+    maior = num_3
+
+# Menor número:
+menor = num_1
+if num_2 < menor:
+    menor = num_2
+
+if num_3 < menor:
+    menor = num_3
+
+
+print(f'The biggiest number is {maior}')
+print(f'The lowest number is {menor}')
